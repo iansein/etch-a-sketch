c69f9adef51b1e4ba5efbecc4b574aeba2e4e918
Ian Sein <ianseinmichel@gmail.com>
2023-10-24 20:03:03 -0300

    Add a pencil and fix a bug
    I added a pencil that appears when you move the cursor on the grid.
    I fixed the bug that was preventing the first square from being painted.

--- functions.js
+++ functions.js
@@ -1,20 +1,26 @@
 const buttonSquares = document.querySelector(".button-squares");
 const gridContainer = document.querySelector(".grid-container");
-let pressedMouse;
-let numberOfSquares = 16;
+gridContainer.classList.add("pencil-cursor");
 
-for (let i = 0; i < numberOfSquares; i++) {
-  let squareContainer = document.createElement("div");
-  squareContainer.classList.add("square-container");
-  gridContainer.appendChild(squareContainer);
-  for (let j = 0; j < numberOfSquares; j++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    squareContainer.appendChild(square);
+window.addEventListener("load", () => {
+  initGrid(16);
+  draw();
+});
+
+function initGrid(numberOfSquares) {
+  for (let i = 0; i < numberOfSquares; i++) {
+    let squareContainer = document.createElement("div");
+    squareContainer.classList.add("square-container");
+    gridContainer.appendChild(squareContainer);
+    for (let j = 0; j < numberOfSquares; j++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      squareContainer.appendChild(square);
+    }
   }
 }
 
-buttonSquares.addEventListener("click", () => {
+function createGrid() {
   numberOfSquares = Number(prompt("Enter the number of squares in the grid"));
   if (numberOfSquares > 0 && numberOfSquares <= 100) {
     let square = document.querySelectorAll(".square");
@@ -28,32 +34,32 @@ buttonSquares.addEventListener("click", () => {
       squareContainer.remove();
     });
 
-    for (let i = 0; i < numberOfSquares; i++) {
-      let squareContainer = document.createElement("div");
-      squareContainer.classList.add("square-container");
-      gridContainer.appendChild(squareContainer);
-      for (let j = 0; j < numberOfSquares; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        squareContainer.appendChild(square);
-      }
-    }
+    initGrid(numberOfSquares);
   }
+}
+
+function draw() {
   const square = document.querySelectorAll(".square");
-  document.addEventListener("mousedown", (event) => {
-    event.preventDefault();
-    pressedMouse = true;
-  });
-  document.addEventListener("mouseup", (event) => {
-    event.preventDefault();
-    pressedMouse = false;
-  });
+
   square.forEach((square) => {
+    square.addEventListener("mousedown", (event) => {
+      event.preventDefault();
+      pressedMouse = true;
+      square.style.backgroundColor = "black";
+    });
+    square.addEventListener("mouseup", (event) => {
+      event.preventDefault();
+      pressedMouse = false;
+    });
     square.addEventListener("mouseover", () => {
-      let mouseOver = true;
       if (pressedMouse) {
         square.style.backgroundColor = "black";
       }
     });
   });
+}
+
+buttonSquares.addEventListener("click", () => {
+  createGrid();
+  draw();
 });
